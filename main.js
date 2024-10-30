0ecbb9bfb4f74c7e6ebfe671ae0eaac70e921520
Savina Shen (Manpower Services Taiwan Co Ltd) <a-savinashen@microsoft.com>
2024-10-30 18:19:54 +0800

    add new controls and descriptions

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const key = import.meta.env.VITE_AZURE_MAPS_KEY;
 const DATA_PATH = `https://gist.githubusercontent.com/ilyabo/68d3dba61d86164b940ffe60e9d36931/raw/a72938b5d51b6df9fa7bba9aa1fb7df00cd0f06a`;
 
 let deck, locations, flows;
+let map;
 
 function onload () {
   async function fetchData() {
@@ -44,7 +45,7 @@ function onload () {
       {pad: 0.3}
     );
 
-    const map = new atlas.Map("myMap", {
+    map = new atlas.Map("myMap", {
       authOptions: {
         authType: atlas.AuthenticationType.subscriptionKey,
         subscriptionKey: key,
@@ -56,6 +57,14 @@ function onload () {
       zoom: initialViewState.zoom,
       bearing: initialViewState.bearing,
       pitch: initialViewState.pitch,
+    });
+
+    map.events.add("ready", () => {
+      map.controls.add(new atlas.control.StyleControl({
+        mapStyles: ['road', 'grayscale_light', 'grayscale_dark', 'night', 'satellite'],
+      }), {
+        position: "top-right",
+      });
     });
 
     deck = new Deck({
@@ -81,6 +90,8 @@ function onload () {
     document.querySelectorAll(".control").forEach((control) => {
       control.onchange = addLayer;
     });
+    document.getElementById("darkMode").onchange = onDarkModeChange;
+
     const flowmap = document.getElementById("deck-canvas");
     const mapContainer = document.getElementById("myMap");
     mapContainer.insertBefore(flowmap, mapContainer.querySelector(".atlas-control-container"));
@@ -94,7 +105,9 @@ function addLayer() {
         id: "my-flowmap-layer",
         data: {locations, flows},
         pickable: true,
+        darkMode: getIsChecked("darkMode"),
         colorScheme: getSelectValue("colorScheme"),
+        clusteringEnabled: getIsChecked("clusteringEnabled"),
         getLocationId: (loc) => loc.id,
         getLocationLat: (loc) => loc.lat,
         getLocationLon: (loc) => loc.lon,
@@ -108,9 +121,11 @@ function addLayer() {
   });
 }
 
-function getSelectValue(id) {
-  var elm = document.getElementById(id);
-  return elm.options[elm.selectedIndex].value;
+function onDarkModeChange() {
+  map.setStyle({style: getIsChecked("darkMode") ? "grayscale_dark" : "grayscale_light"});
+  document.getElementById("deck-canvas").style.mixBlendMode = getIsChecked("darkMode") ? "screen" : "darken";
+  document.getElementById("container").style.backgroundColor = getIsChecked("darkMode") ? "#000" : "#fff";
+  addLayer();
 }
 
 function updateTooltip(state) {
@@ -119,8 +134,8 @@ function updateTooltip(state) {
     tooltip.style.display = "none";
     return;
   }
-    tooltip.style.left = state.position.left;
-    tooltip.style.top = state.position.top;
+    tooltip.style.left = `${state.position.left}px`;
+    tooltip.style.top = `${state.position.top}px`  
     tooltip.innerHTML = state.content;
     tooltip.style.display = "block";
 }
@@ -155,6 +170,14 @@ function getTooltipState(info) {
       };
   }
   return undefined;
+}
+
+function getSelectValue(id) {
+  var elm = document.getElementById(id);
+  return elm.options[elm.selectedIndex].value;
+}
+function getIsChecked(id) {
+  return document.getElementById(id).checked;
 }
 
 document.body.onload = onload;
